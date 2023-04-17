b759953eee298739d4ffa0039886d6757b3e38ed
Marvinmartain <122340360+Marvinmartain@users.noreply.github.com>
2023-04-17 17:46:52 -0400

    redo

--- app.js
+++ app.js
@@ -1,8 +1,23 @@
 const express = require('express');
 const app = express();
 const Log = require('./models/logs');
+const fetch = require('node-fetch');
 
 // GET all logs
+app.get('/news', async (req, res) => {
+    const apiKey = '<YOUR_NEWS_API_KEY>';
+    const apiUrl = `https://newsapi.org/v2/top-headlines?country=us&apiKey=${apiKey}`;
+    
+    try {
+      const response = await fetch(apiUrl);
+      const data = await response.json();
+      res.send(data.articles);
+    } catch (error) {
+      console.error(error);
+      res.status(500).send('Error fetching news data');
+    }
+  }); 
+  
 app.get('/logs', async (req, res) => {
   try {
     const logs = await Log.find();
