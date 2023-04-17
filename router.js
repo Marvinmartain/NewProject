c336036d8a36f0f3a72eef5d2a8d4f7d8184594b
Marvinmartain <122340360+Marvinmartain@users.noreply.github.com>
2023-04-17 15:26:08 -0400

    second

--- router.js
+++ router.js
@@ -1,4 +1,27 @@
+const express = require("express");
+const router = express.Router();
+const User = require("../models/user");
 
+// Get all users
+router.get("/users", async (req, res) => {
+  try {
+    const users = await User.find();
+    res.json(users);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
+
+// Get a single user
+router.get("/users/:id", getUser, (req, res) => {
+  res.json(res.user);
+});
+
+// Create a user
+router.post("/users", async (req, res) => {
+  const user = new User({
+    username: req.body.username,
+    email: req.body.email,
     password: req.body.password
   });
 
