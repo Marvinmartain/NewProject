22b3f46fdc0cceae4dd809b1a8d64b39e9b73575
Marvinmartain <122340360+Marvinmartain@users.noreply.github.com>
2023-04-17 18:09:06 -0400

    notworking

--- logs.js
+++ logs.js
@@ -1,79 +1,47 @@
 const express = require('express');
 const router = express.Router();
-const Log = require('../models/log');
+const Log = require('../models/Log');
 
+// index route
 router.get('/', async (req, res) => {
-  try {
-    const logs = await Log.find();
-    res.render('index', { logs });
-  } catch (err) {
-    console.error(err);
-    res.status(500).send('Server Error');
-  }
+  const logs = await Log.find();
+  res.render('index', { logs });
 });
 
+// new route
 router.get('/new', (req, res) => {
-  res.render('new', { log: new Log() });
+  res.render('new');
 });
 
+// create route
 router.post('/', async (req, res) => {
-  const log = new Log({
-    title: req.body.title,
-    body: req.body.body
-  });
-  try {
-    const newLog = await log.save();
-    res.redirect(`/logs/${newLog.id}`);
-  } catch (err) {
-    console.error(err);
-    res.render('new', {
-      log,
-      errorMessage: 'Error creating log'
-    });
-  }
+  const log = new Log(req.body);
+  await log.save();
+  res.redirect('/logs');
 });
 
+// show route
 router.get('/:id', async (req, res) => {
-  try {
-    const log = await Log.findById(req.params.id);
-    res.render('show', { log });
-  } catch (err) {
-    console.error(err);
-    res.status(500).send('Server Error');
-  }
+  const log = await Log.findById(req.params.id);
+  res.render('show', { log });
 });
 
+// edit route
 router.get('/:id/edit', async (req, res) => {
-  try {
-    const log = await Log.findById(req.params.id);
-    res.render('edit', { log });
-  } catch (err) {
-    console.error(err);
-    res.status(500).send('Server Error');
-  }
+  const log = await Log.findById(req.params.id);
+  res.render('edit', { log });
 });
 
+// update route
 router.put('/:id', async (req, res) => {
-  try {
-    let log = await Log.findById(req.params.id);
-    log.title = req.body.title;
-    log.body = req.body.body;
-    await log.save();
-    res.redirect(`/logs/${log.id}`);
-  } catch (err) {
-    console.error(err);
-    res.status(500).send('Server Error');
-  }
+  const log = await Log.findByIdAndUpdate(req.params.id, req.body, { new: true });
+  res.redirect(`/logs/${log.id}`);
 });
 
+// delete route
 router.delete('/:id', async (req, res) => {
-  try {
-    await Log.findByIdAndDelete(req.params.id);
-    res.redirect('/logs');
-  } catch (err) {
-    console.error(err);
-    res.status(500).send('Server Error');
-  }
+  await Log.findByIdAndDelete(req.params.id);
+  res.redirect('/logs');
 });
 
 module.exports = router;
